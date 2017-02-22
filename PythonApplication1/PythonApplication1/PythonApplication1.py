89918cfba43175b82c532788f9da708813e01010
SlapTurtle <michaeldkirkegaard@gmail.com>
2017-02-22 16:00:36 +0100

    First Commit

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,18 @@
+#import tensorflow as tf
+from TwitterAPI import TwitterAPI
+
+def test():
+    consumer_key = 'o5aaDQNePqeOwLJpXFC1qeWmL'
+    consumer_secret = 'TM8g6A7GveM5wkZahV8ZgILYGHJcChVNVJblAEn6ZdL9gLCVnA'
+    access_token_key = '3688759636-pYAsFBCIAFphzdwvNZjrbcydWFLZ2NFogRybaGc'
+    access_token_secret = 'hmM8DZAnLMGn7lGrGbU697eIB0hwbK8XheUXSIQM5CruM'
+    api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+    r = api.request('search/tweets', {'q':'pizza'})
+    for item in r.get_iterator():
+        if 'text' in item:
+            print(item['text'].encode('utf-8'))
+        elif 'message' in item:
+            print('{} ({})'.format(item['message'], item['code']))
+
+if __name__ == '__main__':
+    test()
