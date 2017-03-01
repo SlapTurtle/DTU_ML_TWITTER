9393c85710e57f98e1ca56844d2ef69eba5adc8b
SlapTurtle <michaeldkirkegaard@gmail.com>
2017-03-01 16:56:19 +0100

    +write to data.txt todo: Datechange

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -3,7 +3,13 @@ from TwitterAPI import TwitterAPI
 import datetime
 import os
 
+consumer_key = 'o5aaDQNePqeOwLJpXFC1qeWmL'
+consumer_secret = 'TM8g6A7GveM5wkZahV8ZgILYGHJcChVNVJblAEn6ZdL9gLCVnA'
+access_token_key = '3688759636-pYAsFBCIAFphzdwvNZjrbcydWFLZ2NFogRybaGc'
+access_token_secret = 'hmM8DZAnLMGn7lGrGbU697eIB0hwbK8XheUXSIQM5CruM'
+
 SEARCHTAG_PATH = 'Files\searchtags.txt'
+DATA_PATH = 'Files\data.txt'
 
 def getSearchTags():
     tags = []
@@ -20,26 +26,30 @@ def getSearchTags():
     return tags
 
 
-def test():
-    consumer_key = 'o5aaDQNePqeOwLJpXFC1qeWmL'
-    consumer_secret = 'TM8g6A7GveM5wkZahV8ZgILYGHJcChVNVJblAEn6ZdL9gLCVnA'
-    access_token_key = '3688759636-pYAsFBCIAFphzdwvNZjrbcydWFLZ2NFogRybaGc'
-    access_token_secret = 'hmM8DZAnLMGn7lGrGbU697eIB0hwbK8XheUXSIQM5CruM'
+def readData():
     api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
-    '''r = api.request('search/tweets', {'q':'pizza'})
-    for item in r.get_iterator():
-        if 'text' in item:
-            print(item['text'].encode('utf-8'))
-        elif 'message' in item:
-            print('{} ({})'.format(item['message'], item['code']))
-    '''
-    r = api.request('statuses/filter', {'track':{*getSearchTags()}})
-    for item in r.get_iterator():
-        '''if 'created_at' in item:
-            print(item['created_at'])            
-            print(datetime.datetime.now())'''
-        if 'text' in item:
-            print(item['text'].encode('utf-8'))
+    path = getDataPath()
+    searchTags = *getSearchTags()
+
+    r = api.request('statuses/filter', {'track':{searchTags}})
+
+    with open(path, "a") as myfile:
+        for item in r.get_iterator():
+            if 'created_at' and 'text' in item:
+                s = '[{}] {}\n'.format(item['created_at'], item['text'].encode('utf-8'))
+                myfile.write(s)
+            
+
+def getDataPath():
+    path = os.path.dirname(os.path.abspath(__file__))
+    if type(path) == str:
+        i,j = len(path),0
+        while (j!=2):
+            if path[i-1] == '\\':
+                j = j + 1
+            i = i-1
+        path = path[0:i+1] + DATA_PATH
+    return path
 
 if __name__ == '__main__':
-    test()
+    readData()
