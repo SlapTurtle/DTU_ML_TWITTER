498300bfbb907a044141ac5c61a088ae8cb413c7
SlapTurtle <michaeldkirkegaard@gmail.com>
2017-03-01 14:55:57 +0100

    stream

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -7,12 +7,17 @@ def test():
     access_token_key = '3688759636-pYAsFBCIAFphzdwvNZjrbcydWFLZ2NFogRybaGc'
     access_token_secret = 'hmM8DZAnLMGn7lGrGbU697eIB0hwbK8XheUXSIQM5CruM'
     api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
-    r = api.request('search/tweets', {'q':'pizza'})
+    '''r = api.request('search/tweets', {'q':'pizza'})
     for item in r.get_iterator():
         if 'text' in item:
             print(item['text'].encode('utf-8'))
         elif 'message' in item:
             print('{} ({})'.format(item['message'], item['code']))
+    '''
+    r = api.request('statuses/filter', {'track':'pizza'})
+    for item in r.get_iterator():
+        if 'text' in item:
+            print(item['text'].encode('utf-8'))
 
 if __name__ == '__main__':
     test()
