19f5ed390dfb4a179547ac06a815ad93588b555e
SlapTurtle <michaeldkirkegaard@gmail.com>
2017-03-01 16:12:36 +0100

    + searchtags implemented TODO: write to file(s)

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,6 +1,24 @@
 #import tensorflow as tf
 from TwitterAPI import TwitterAPI
 import datetime
+import os
+
+SEARCHTAG_PATH = 'Files\searchtags.txt'
+
+def getSearchTags():
+    tags = []
+    path = os.path.dirname(os.path.abspath(__file__))
+    if type(path) == str:
+        i,j = len(path),0
+        while (j!=2):
+            if path[i-1] == '\\':
+                j = j + 1
+            i = i-1
+        path = path[0:i+1] + SEARCHTAG_PATH
+        for line in open(path).readlines():
+            tags.append(line[:-1])
+    return tags
+
 
 def test():
     consumer_key = 'o5aaDQNePqeOwLJpXFC1qeWmL'
@@ -15,7 +33,7 @@ def test():
         elif 'message' in item:
             print('{} ({})'.format(item['message'], item['code']))
     '''
-    r = api.request('statuses/filter', {'track':{'Bill Gates', }})
+    r = api.request('statuses/filter', {'track':{*getSearchTags()}})
     for item in r.get_iterator():
         '''if 'created_at' in item:
             print(item['created_at'])            
