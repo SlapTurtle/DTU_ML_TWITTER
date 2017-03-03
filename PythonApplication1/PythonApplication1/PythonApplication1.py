6725208a190dfb22c09088c4ead2dfbd4846e86c
SlapTurtle <michaeldkirkegaard@gmail.com>
2017-03-03 16:10:06 +0100

    Datagrab (almost) complete + auto data / storage + stop med enter todo: auto reconnect

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -2,16 +2,17 @@
 from TwitterAPI import TwitterAPI
 import datetime
 import os
+from threading import Thread
 
 consumer_key = 'o5aaDQNePqeOwLJpXFC1qeWmL'
 consumer_secret = 'TM8g6A7GveM5wkZahV8ZgILYGHJcChVNVJblAEn6ZdL9gLCVnA'
 access_token_key = '3688759636-pYAsFBCIAFphzdwvNZjrbcydWFLZ2NFogRybaGc'
 access_token_secret = 'hmM8DZAnLMGn7lGrGbU697eIB0hwbK8XheUXSIQM5CruM'
 
-SEARCHTAG_PATH = 'Files\\searchtags.txt'
-DATA_PATH = 'Files\\data.txt'
+DATA_PATH = 'Files\\'
+SEARCHTAG_PATH = 'searchtags.txt'
 
-def directorySkip(s="Files\\"):
+def directorySkip(s=DATA_PATH):
     path = os.path.dirname(os.path.abspath(__file__))
     if type(path) == str:
         i,j = len(path),0
@@ -23,29 +24,39 @@ def directorySkip(s="Files\\"):
     return None
 
 def getSearchTags():
-    path = directorySkip(SEARCHTAG_PATH)
-    if (path):
-        tags = set()
-        for line in open(path).readlines():
-            tags.add(line[:-1])
-        return tags
-    return None
+    return directorySkip() + SEARCHTAG_PATH
 
-def getDataPath(s=DATA_PATH):
-    return directorySkip(s)
+def getDataPath(s):
+    path = directorySkip() + s[-4:]+s[4:7]+s[8:10]+'.txt' #fixed string index's
+    open('myfile.dat', 'w+').close()
+    return path
 
-def readData():
+def readData(t):
     api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
-    path = getDataPath()
+    path = ''
     searchTags = getSearchTags()
+    r = api.request('statuses/filter', {'track':searchTags, 'language':{'en'}})
 
-    r = api.request('statuses/filter', {'track':searchTags})
+    for item in r.get_iterator():
+        if 'created_at' and 'text' in item:
+            path = getDataPath(item['created_at'])
+            s = '[{}] {}\n'.format(item['created_at'], item['text'].encode('utf-8'))
+            with open(path, "a") as file:
+                file.write(s)
+        else:
+            print(item)
+        if(not t.isAlive()):
+            break
+    print("stopping data")
 
-    with open(path, "a") as myfile:
-        for item in r.get_iterator():
-            if 'created_at' and 'text' in item:
-                s = '[{}] {}\n'.format(item['created_at'], item['text'].encode('utf-8'))
-                myfile.write(s)
+def getInput():
+    print("start")
+    input()
 
 if __name__ == '__main__':
-    readData()
+    t = Thread(target = getInput)
+    t2 = Thread(target = readData, args={t})
+    t.start()
+    t2.start()
+    t.join()
+    t2.join()
