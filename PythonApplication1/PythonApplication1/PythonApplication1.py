5f42585d41fe36ba3cf2db569d1c8d06b0b8a01c
nickl <nickl@DESKTOP-TGOSANT>
2017-03-06 17:19:49 +0100

    d

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -41,7 +41,6 @@ def readData(t):
     api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
     path = ''
     searchTags = getSearchTags()
-    print(searchTags)
     r = api.request('statuses/filter', {'track':searchTags, 'language':{'en'}})
     printStatus(searchTags, r.status_code)
     for item in r.get_iterator():
