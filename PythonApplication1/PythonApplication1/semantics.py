13f74634f49dd6cca323ad9a2ecc3d48fe85f232
Nicklas Hansen <nicklash@hotmail.dk>
2017-03-09 23:26:42 +0100

    semantics

--- PythonApplication1/PythonApplication1/semantics.py
+++ PythonApplication1/PythonApplication1/semantics.py
@@ -1,4 +1,40 @@
+from PythonApplication1 import *
+from preprocessing import *
+
+FILE = "testdata"
+POSITIVES = []
+NEGATIVES = []
 
 def semantics():
-    print("running semantics")
+    global POSITIVE, NEGATIVES
+    print("Running Semantics...")
+    data = open(getDataPath(FILE))
+    POSITIVES = set(loadLexicon("positives"))
+    NEGATIVES = set(loadLexicon("negatives"))
 
+    for line in data:
+        result = evaluate(line)
+        print(result)
+        #filedump(evaluate(line))
+
+
+def evaluate(line):
+    list = line.split()
+    positives = 0
+    negatives = 0
+    total = len(list)
+
+    for word in list:
+        if word in POSITIVES:
+            positives = positives + 1
+        elif word in NEGATIVES:
+            negatives = negatives + 1
+    
+    return positives - negatives
+
+def loadLexicon(s):
+    list = []
+    with open(getDataPath(s), "r") as file:
+        for line in file:
+            list.append(line[:-1])
+    return list
